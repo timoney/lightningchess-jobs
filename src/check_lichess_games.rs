f5b805966388e0abf7a7854f63969670fc397208
Matt <timoney.matt@gmail.com>
2023-01-25 16:28:00 -0500

    add tests

--- src/check_lichess_games.rs
+++ src/check_lichess_games.rs
@@ -5,6 +5,22 @@ use sqlx::{Pool, Postgres};
 use sqlx::postgres::PgPoolOptions;
 use crate::models::{Challenge, LightningChessResult, LichessExportGameResponse};
 
+fn get_winner_username(challenge: &Challenge, winner: &str) -> String {
+    // determine if user who created the challenge won
+    let creator_won_black = winner == "black" && challenge.color.as_ref().unwrap() == "black";
+    let creator_won_white = winner == "white" && challenge.color.as_ref().unwrap() == "white";
+    return if creator_won_black || creator_won_white {
+        &challenge.username
+    } else {
+        &challenge.opp_username
+    }.to_string()
+}
+
+fn calculate_fee_per_person(challenge: &Challenge) -> i64 {
+    let initial_fee: f64 = (challenge.sats.unwrap() as f64) * 0.02;
+    initial_fee.floor() as i64
+}
+
 async fn check(pool: &Pool<Postgres>) -> LightningChessResult<usize> {
     let admin = env::var("ADMIN_ACCOUNT").unwrap();
 
@@ -46,12 +62,9 @@ async fn check(pool: &Pool<Postgres>) -> LightningChessResult<usize> {
             continue;
         }
 
-        // determine fee
-        let initial_fee: f64 = (challenge.sats.unwrap() as f64) * 0.02;
-        let rounded_down = initial_fee.floor() as i64;
-        // make even
-        let fee = rounded_down - rounded_down % 2;
-        println!("fee {}", fee);
+        let fee_per_person = calculate_fee_per_person(challenge);
+        println!("fee per person{}", fee_per_person);
+        let total_fee = fee_per_person * 2;
 
         let mut tx = pool.begin().await?;
 
@@ -63,14 +76,14 @@ async fn check(pool: &Pool<Postgres>) -> LightningChessResult<usize> {
             .bind(&admin)
             .bind(admin_ttype)
             .bind(admin_detail)
-            .bind(fee)
+            .bind(total_fee)
             .bind(admin_state)
             .bind(&challenge.lichess_challenge_id.as_ref().unwrap())
             .execute(&mut tx).await?;
         println!("insert admin transaction");
 
         sqlx::query( "UPDATE lightningchess_balance set balance=balance + $1 WHERE username=$2")
-            .bind(fee)
+            .bind(total_fee)
             .bind(&admin)
             .execute(&mut tx).await?;
         println!("update admin balance");
@@ -78,13 +91,13 @@ async fn check(pool: &Pool<Postgres>) -> LightningChessResult<usize> {
         let winner = lichess_export_game_response.winner.get_or_insert("".to_string());
         if winner == "black" || winner == "white" {
             // pay money to winner
-            let winner_username = if challenge.color.as_ref().unwrap() == "black" { &challenge.username } else { &challenge.opp_username };
+            let winner_username = get_winner_username(&challenge, winner);
             let winner_ttype = "winnings";
             let winner_detail = "";
-            let winning_amt = (&challenge.sats.unwrap() * 2) - fee;
+            let winning_amt = (&challenge.sats.unwrap() * 2) - total_fee;
             let winner_state = "SETTLED";
             sqlx::query( "INSERT INTO lightningchess_transaction (username, ttype, detail, amount, state) VALUES ($1, $2, $3, $4, $5)")
-                .bind(winner_username)
+                .bind(&winner_username)
                 .bind(winner_ttype)
                 .bind(winner_detail)
                 .bind(winning_amt)
@@ -94,14 +107,14 @@ async fn check(pool: &Pool<Postgres>) -> LightningChessResult<usize> {
 
             sqlx::query( "UPDATE lightningchess_balance set balance=balance + $1 WHERE username=$2")
                 .bind(winning_amt)
-                .bind(winner_username)
+                .bind(&winner_username)
                 .execute(&mut tx).await?;
             println!("update winner balance");
         } else {
             // no winner so return money to both people
             let draw_ttype = "draw";
             let draw_detail = "initial sats amount minus 2% fee";
-            let draw_amt = &challenge.sats.unwrap() - (fee / 2);
+            let draw_amt = &challenge.sats.unwrap() - fee_per_person;
             let draw_state = "SETTLED";
             sqlx::query( "INSERT INTO lightningchess_transaction (username, ttype, detail, amount, state) VALUES ($1, $2, $3, $4, $5)")
                 .bind(&challenge.username)
@@ -179,5 +192,65 @@ pub async fn check_lichess_games() -> () {
         println!("sleeping for {duration:?}");
         sleep(duration).await;
         loop_count = loop_count + 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_challenge() -> Challenge {
+        Challenge { id: 1,
+            username: "user1".to_string(),
+            time_limit: None,
+            opponent_time_limit: None,
+            increment: None,
+            color: Some("white".to_string()),
+            sats: Some(100),
+            opp_username: "user2".to_string(),
+            status: None,
+            lichess_challenge_id: None,
+            created_on: None,
+            expire_after: None
+        }
+    }
+
+    #[test]
+    fn winner_creator_as_white() {
+        let challenge = get_challenge();
+
+        // creator won as white
+        assert_eq!(get_winner_username(&challenge, "white"), "user1");
+        // creator lost as white
+        assert_eq!(get_winner_username(&challenge, "black"), "user2");
+    }
+
+    #[test]
+    fn winner_creator_as_black() {
+        let mut challenge = get_challenge();
+        challenge.color = Some("black".to_string());
+
+        // creator lost as black
+        assert_eq!(get_winner_username(&challenge, "white"), "user2");
+        // creator won as black
+        assert_eq!(get_winner_username(&challenge, "black"), "user1");
+    }
+
+    #[test]
+    fn calculate_fee_per_person_test() {
+        let mut challenge = get_challenge();
+        assert_eq!(calculate_fee_per_person(&challenge), 2);
+        challenge.sats = Some(101);
+        assert_eq!(calculate_fee_per_person(&challenge), 2);
+        challenge.sats = Some(110);
+        assert_eq!(calculate_fee_per_person(&challenge), 2);
+        challenge.sats = Some(149);
+        assert_eq!(calculate_fee_per_person(&challenge), 2);
+        challenge.sats = Some(150);
+        assert_eq!(calculate_fee_per_person(&challenge), 3);
+        challenge.sats = Some(199);
+        assert_eq!(calculate_fee_per_person(&challenge), 3);
+        challenge.sats = Some(200);
+        assert_eq!(calculate_fee_per_person(&challenge), 4);
     }
 }
